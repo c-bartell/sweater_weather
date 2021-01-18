bfd82bd287832cb5a8ce5d24f4e258100a12f944
Curtis Bartell <cbartell980@gmail.com>
2021-01-18 13:23:49 -0700

    Add test for Location PORO attributes

--- spec/poros/location_spec.rb
+++ spec/poros/location_spec.rb
@@ -10,4 +10,12 @@ describe 'Location PORO' do
   it 'exists' do
     expect(Location.new(@data)).to be_a Location
   end
+
+  it 'has attributes' do
+    location = Location.new(@data)
+    lat_lng = @data[:results][0][:locations][0][:latLng]
+
+    expect(location.latitude).to eq(lat_lng[:lat])
+    expect(location.longitude).to eq(lat_lng[:lng])
+  end
 end
