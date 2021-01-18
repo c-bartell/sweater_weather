2783b08e490854f4a4b0070dc8fde2e6fdcbff2c
Curtis Bartell <cbartell980@gmail.com>
2021-01-18 15:44:28 -0700

    Add current_weather method to Forecast

--- app/poros/forecast.rb
+++ app/poros/forecast.rb
@@ -1,11 +1,25 @@
 class Forecast
-  attr_reader :current_weather,
-              :daily_weather,
+  attr_reader :daily_weather,
               :hourly_weather
 
   def initialize(data)
     @current_weather = data[:current]
     @daily_weather = data[:daily][1..5]
     @hourly_weather = data[:hourly][1..8]
+  end
+
+  def current_weather
+    {
+      datetime: @current_weather[:dt],
+      sunrise: @current_weather[:sunrise],
+      sunset: @current_weather[:sunset],
+      temperature: @current_weather[:temperature],
+      feels_like: @current_weather[:feels_like],
+      humidity: @current_weather[:humidity],
+      uvi: @current_weather[:uvi],
+      visibility: @current_weather[:visibility],
+      conditions: @current_weather[:weather][0][:description],
+      icon: @current_weather[:weather][0][:icon],
+    }
   end
 end
