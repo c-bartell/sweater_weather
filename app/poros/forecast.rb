f4e794da93fd1e069160cbde102a47842bb00193
Curtis Bartell <cbartell980@gmail.com>
2021-01-18 15:16:51 -0700

    Add attributes to Forecast

--- app/poros/forecast.rb
+++ app/poros/forecast.rb
@@ -1,7 +1,11 @@
 class Forecast
-  attr_reader :current_weather
+  attr_reader :current_weather,
+              :daily_weather,
+              :hourly_weather
 
   def initialize(data)
-    
+    @current_weather = data[:current]
+    @daily_weather = data[:daily][1..5]
+    @hourly_weather = data[:hourly][1..8]
   end
 end
