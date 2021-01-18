1b3066b02693c62e12fbaa8c8dc1bf47370bc3cf
Curtis Bartell <cbartell980@gmail.com>
2021-01-18 13:24:15 -0700

    Add latitude and longitude to location

--- app/poros/location.rb
+++ app/poros/location.rb
@@ -1,5 +1,9 @@
 class Location
-  def initialize(data)
+  attr_reader :latitude,
+              :longitude
 
+  def initialize(data)
+    @latitude = data[:results][0][:locations][0][:latLng][:lat]
+    @longitude = data[:results][0][:locations][0][:latLng][:lng]
   end
 end
