656352e9f125fe45c9e0b0c237e4cdae42c7bcc6
Curtis Bartell <cbartell980@gmail.com>
2021-01-17 17:48:52 -0700

    Refactor location_to_coords

--- app/services/geocode_service.rb
+++ app/services/geocode_service.rb
@@ -1,7 +1,20 @@
 class GeocodeService
   class << self
     def location_to_coords(location)
-      conn = Faraday.new(
+      response = conn.post('address') do |req|
+        req.body = {
+          location: location,
+          options: options
+        }.to_json
+      end
+
+      JSON.parse(response.body, symbolize_names: true)
+    end
+
+    private
+
+    def conn
+      Faraday.new(
         url: 'https://www.mapquestapi.com/geocoding/v1',
         params: {
           key: ENV['GEOCODE_API_KEY']
@@ -11,18 +24,14 @@ class GeocodeService
           'Accept' => 'application/json'
         }
       )
-      response = conn.post('address') do |req|
-        req.body = {
-          location: location,
-          options: {
-            thumbMaps: false,
-            maxResults: 1,
-            intlMode: "AUTO"
-          }
-        }.to_json
-      end
+    end
 
-      JSON.parse(response.body, symbolize_names: true)
+    def options
+      {
+        thumbMaps: false,
+        maxResults: 1,
+        intlMode: 'AUTO'
+      }
     end
   end
 end
