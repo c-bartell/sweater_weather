97666d59e2c105ced2b74332ef6469ef56df1102
Curtis Bartell <cbartell980@gmail.com>
2021-01-18 15:57:50 -0700

    Add format_datetime

--- app/poros/forecast.rb
+++ app/poros/forecast.rb
@@ -10,16 +10,20 @@ class Forecast
 
   def current_weather
     {
-      datetime: @current_weather[:dt],
-      sunrise: @current_weather[:sunrise],
-      sunset: @current_weather[:sunset],
+      datetime: format_datetime(@current_weather[:dt]),
+      sunrise: format_datetime(@current_weather[:sunrise]),
+      sunset: format_datetime(@current_weather[:sunset]),
       temperature: @current_weather[:temperature],
       feels_like: @current_weather[:feels_like],
       humidity: @current_weather[:humidity],
       uvi: @current_weather[:uvi],
       visibility: @current_weather[:visibility],
       conditions: @current_weather[:weather][0][:description],
-      icon: @current_weather[:weather][0][:icon],
+      icon: @current_weather[:weather][0][:icon]
     }
+  end
+
+  def format_datetime(seconds)
+    Time.at(seconds).to_s
   end
 end
