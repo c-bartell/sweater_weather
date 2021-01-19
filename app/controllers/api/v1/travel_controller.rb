66d43a9d3b8e24fef9d521d3f98df819f4dea2f9
Curtis Bartell <cbartell980@gmail.com>
2021-01-19 10:07:16 -0700

    Use strings in trip request call

--- app/controllers/api/v1/travel_controller.rb
+++ app/controllers/api/v1/travel_controller.rb
@@ -1,7 +1,21 @@
 class Api::V1::TravelController < ApplicationController
   def munchies
-    start_point = GeocodeFacade.location(params[:start])
-    end_point = GeocodeFacade.location(params[:end])
+    # start_point = GeocodeFacade.location(params[:start])
+    # end_point = GeocodeFacade.location(params[:end])
+    conn = Faraday.new(
+      url: 'https://www.mapquestapi.com/directions/v2',
+      params: {
+        key: ENV['GEOCODE_API_KEY']
+      },
+      headers: {
+        'Content-Type' => 'application/json',
+        'Accept' => 'application/json'
+      }
+    )
+    trip = conn.get('route') do |req|
+      req.params[:from] = params[:start]
+      req.params[:to] = params[:end]
+    end
     binding.pry
   end
 end
