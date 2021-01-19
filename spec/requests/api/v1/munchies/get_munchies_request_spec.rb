7babbe746b8a15841908ae6c53cd49463adf5956
Curtis Bartell <cbartell980@gmail.com>
2021-01-19 09:45:27 -0700

    Set up cassettes for geocode requests

--- spec/requests/api/v1/munchies/get_munchies_request_spec.rb
+++ spec/requests/api/v1/munchies/get_munchies_request_spec.rb
@@ -2,36 +2,40 @@ require 'rails_helper'
 
 describe 'Munchies request' do
   it 'can can retrieve food and forecast info for a destination city' do
-    start_point = 'denver,co'
-    end_point = 'pueblo,co'
-    food = 'chinese'
-    headers = {
-      "Content-Type" => 'application/json',
-      "Accept" => 'application/json'
-    }
+    VCR.use_cassette('puebloco_coord_request') do
+      VCR.use_cassette('denverco_coord_request') do
+        start_point = 'denver,co'
+        end_point = 'pueblo,co'
+        food = 'chinese'
+        headers = {
+          "Content-Type" => 'application/json',
+          "Accept" => 'application/json'
+        }
 
-    get api_v1_munchies_path(start: start_point, end: end_point, food: food), headers: headers
+        get api_v1_munchies_path(start: start_point, end: end_point, food: food), headers: headers
 
-    expect(response).to be_successful
+        expect(response).to be_successful
 
-    data = JSON.parse(response.body, symbolize_names: true)
+        data = JSON.parse(response.body, symbolize_names: true)
 
-    expect(data[:id]).to be nil
-    expect(data[:type]).to eq 'munchie'
+        expect(data[:id]).to be nil
+        expect(data[:type]).to eq 'munchie'
 
-    attributes = data[:attributes]
+        attributes = data[:attributes]
 
-    expect(attributes[:destination_city]).to eq 'Pueblo, CO'
-    expect(attributes[:travel_time]).to eq '1 hours 48 min'
+        expect(attributes[:destination_city]).to eq 'Pueblo, CO'
+        expect(attributes[:travel_time]).to eq '1 hours 48 min'
 
-    forecast = attributes[:forecast]
+        forecast = attributes[:forecast]
 
-    expect(forecast[:summary]).to be_a String
-    expect(forecast[:temperature]).to be_a String
+        expect(forecast[:summary]).to be_a String
+        expect(forecast[:temperature]).to be_a String
 
-    restaurant = attributes[:restaurant]
+        restaurant = attributes[:restaurant]
 
-    expect(restaurant[:name]).to be_a String
-    expect(restaurant[:address]).to be_a String
+        expect(restaurant[:name]).to be_a String
+        expect(restaurant[:address]).to be_a String
+      end
+    end
   end
 end
