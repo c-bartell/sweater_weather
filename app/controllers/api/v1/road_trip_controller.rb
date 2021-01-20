2aeba4dc94a50697c0c7a6a37e2a40c63c2f5797
Curtis Bartell <cbartell980@gmail.com>
2021-01-20 03:40:42 -0700

    Refactor with serializer

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,19 +1,12 @@
 class Api::V1::RoadTripController < ApplicationController
   def create
-    road_trip = RoadTripFacade.road_trip(params[:origin], params[:end_point])
-    payload = {
-      data: {
-        id: nil,
-        type: 'roadtrip',
-        attributes: {
-          start_city: road_trip.start_city,
-          end_city: road_trip.end_city,
-          travel_time: road_trip.travel_time,
-          weather_at_eta: road_trip.weather_at_eta
-        }
-      }
-    }
-    
-    render json: payload
+
+    render json: RoadTripSerializer.new(road_trip)
+  end
+
+  private
+
+  def road_trip
+    @road_trip ||= RoadTripFacade.road_trip(params[:origin], params[:end_point])
   end
 end
