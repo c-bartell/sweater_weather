5cc8310cce59ef0ce42d2888915f47c415b782d3
Curtis Bartell <cbartell980@gmail.com>
2021-01-20 02:45:31 -0700

    Add road trip poro test for trip attributes

---
+++ spec/poros/road_trip_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe 'Road Trip PORO' do
+  before :each do
+    VCR.use_cassette('denver_to_pueblo_travel_request') do
+      start_point = 'Denver,Co'
+      end_point = 'Pueblo,Co'
+      @trip_data = GeocodeService.road_trip_data(start_point, end_point)
+    end
+  end
+
+  it 'has trip data attributes' do
+    road_trip = RoadTrip.new(@trip_data)
+
+    expect(road_trip).to be_a RoadTrip
+    expect(road_trip.id).to be_nil
+    expect(road_trip.start_city).to eq 'Denver, CO'
+    expect(road_trip.end_city).to eq 'Pueblo, CO'
+    expect(road_trip.travel_time).to eq '1 hours 44 min'
+  end
+end
