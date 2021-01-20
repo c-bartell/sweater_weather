084e2751a196a6699ce8cb08b0670e5da5a069b5
Curtis Bartell <cbartell980@gmail.com>
2021-01-20 02:02:28 -0700

    Add road_trip request tests

--- spec/requests/api/v1/road_trip/post_road_trip_request_spec.rb
+++ spec/requests/api/v1/road_trip/post_road_trip_request_spec.rb
@@ -10,47 +10,51 @@ describe 'Road Trip POST request' do
   end
 
   it 'I can get a road trip' do
-    start_point = 'Denver,Co'
-    end_point = 'Pueblo,Co'
-    headers = {
-      'Content-Type' => 'application/json',
-      'Accept' => 'application/json'
-    }
+    VCR.use_cassette('denver_to_pueblo_roadtrip_request') do
+      VCR.use_cassette('puebloco_weather_request') do
+        start_point = 'Denver,Co'
+        end_point = 'Pueblo,Co'
+        headers = {
+          'Content-Type' => 'application/json',
+          'Accept' => 'application/json'
+        }
 
-    post(
-      api_v1_road_trip_path(
-        origin: start_point, end_point: end_point, api_key: @user.api_key
-      ),
-      headers: headers
-    )
+        post(
+          api_v1_road_trip_path(
+            origin: start_point, end_point: end_point, api_key: @user.api_key
+          ),
+          headers: headers
+        )
 
-    expect(response).to be_successful
+        expect(response).to be_successful
 
-    parsed_data = JSON.parse(response.body, symbolize_names: true)
+        parsed_data = JSON.parse(response.body, symbolize_names: true)
 
-    expect(parsed_data).to have_key :data
-    expect(parsed_data[:data]).to be_a Hash
+        expect(parsed_data).to have_key :data
+        expect(parsed_data[:data]).to be_a Hash
 
-    road_trip_data = parsed_data[:data]
+        road_trip_data = parsed_data[:data]
 
-    expect(road_trip_data).to have_key :id
-    expect(road_trip_data[:id]).to be nil
-    expect(road_trip_data).to have_key :type
-    expect(road_trip_data[:type]).to eq 'roadtrip'
+        expect(road_trip_data).to have_key :id
+        expect(road_trip_data[:id]).to be nil
+        expect(road_trip_data).to have_key :type
+        expect(road_trip_data[:type]).to eq 'roadtrip'
 
-    attributes = road_trip_data[:attributes]
+        attributes = road_trip_data[:attributes]
 
-    expect(attributes).to have_key :start_city
-    expect(attributes[:start_city]).to be_a String
-    expect(attributes).to have_key :end_city
-    expect(attributes[:end_city]).to be_a String
-    expect(attributes).to have_key :travel_time
-    expect(attributes[:travel_time]).to be_a String
-    expect(attributes).to have_key :weather_at_eta
-    expect(attributes[:weather_at_eta]).to be_a Hash
-    expect(attributes[:weather_at_eta]).to have_key :temperature
-    expect(attributes[:weather_at_eta][:temperature]).to be_a Float
-    expect(attributes[:weather_at_eta]).to have_key :conditions
-    expect(attributes[:weather_at_eta][:conditions]).to be_a Float
+        expect(attributes).to have_key :start_city
+        expect(attributes[:start_city]).to be_a String
+        expect(attributes).to have_key :end_city
+        expect(attributes[:end_city]).to be_a String
+        expect(attributes).to have_key :travel_time
+        expect(attributes[:travel_time]).to be_a String
+        expect(attributes).to have_key :weather_at_eta
+        expect(attributes[:weather_at_eta]).to be_a Hash
+        expect(attributes[:weather_at_eta]).to have_key :temperature
+        expect(attributes[:weather_at_eta][:temperature]).to be_a Float
+        expect(attributes[:weather_at_eta]).to have_key :conditions
+        expect(attributes[:weather_at_eta][:conditions]).to be_a String
+      end
+    end
   end
 end
