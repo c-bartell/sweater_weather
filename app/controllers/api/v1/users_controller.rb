d25eb886c470b90dabe001387182f7f1e83c10dd
Curtis Bartell <cbartell980@gmail.com>
2021-01-19 21:47:46 -0700

    Add user creation endpoint

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,18 @@ class Api::V1::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     @user.save
-    binding.pry
+    payload = {
+      data: {
+        type: 'users',
+        id: @user.id,
+        attributes: {
+          email: @user.email,
+          api_key: @user.api_key
+        }
+      }
+    }
+    # binding.pry
+    render json: payload, status: 201
   end
 
   private
