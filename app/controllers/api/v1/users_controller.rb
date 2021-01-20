9b644526d58a99250152869c8b4b47379518c149
Curtis Bartell <cbartell980@gmail.com>
2021-01-19 21:53:38 -0700

    Refactor create endpoint with serializer

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,18 +2,8 @@ class Api::V1::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     @user.save
-    payload = {
-      data: {
-        type: 'users',
-        id: @user.id,
-        attributes: {
-          email: @user.email,
-          api_key: @user.api_key
-        }
-      }
-    }
-    # binding.pry
-    render json: payload, status: 201
+    
+    render json: UserSerializer.new(@user), status: 201
   end
 
   private
