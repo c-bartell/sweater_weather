deaafe49ac3d18165e84f12dade47131e08c2938
Curtis Bartell <cbartell980@gmail.com>
2021-01-19 17:57:15 -0700

    Add ImageFacade

--- spec/facades/image_facade_spec.rb
+++ spec/facades/image_facade_spec.rb
@@ -4,8 +4,7 @@ describe 'Image Facade' do
   it 'it can create an image' do
     VCR.use_cassette('denver_background_request') do
       location = 'denver,co'
-      background_data = ImageService.background(location)
-
+      
       expect(ImageFacade.background(location)).to be_an Image
     end
   end
