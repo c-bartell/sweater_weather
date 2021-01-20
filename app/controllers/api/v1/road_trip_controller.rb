c018fb78feeef0264e8258832edb169b0b5297de
Curtis Bartell <cbartell980@gmail.com>
2021-01-20 03:59:09 -0700

    Add 401 response for invalid api key

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,7 +1,12 @@
 class Api::V1::RoadTripController < ApplicationController
   def create
+    if params[:api_key] && User.find_by(api_key: params[:api_key])
 
-    render json: RoadTripSerializer.new(road_trip)
+      render json: RoadTripSerializer.new(road_trip)
+    else
+
+      render json: { errors: ['Invalid API key'] }, status: 401
+    end
   end
 
   private
